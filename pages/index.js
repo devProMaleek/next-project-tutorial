a71f8bccc5c2ec15682607d6c2b7c5345fa1cc9d
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-13 16:05:18 +0100

    Layout Wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,37 @@
-import React from 'react'
-import MeetupList from '../components/meetups/MeetupList'
+import React from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+import Layout from '../components/layout/Layout';
 
 const index = () => {
   const DUMMY_MEETUPS = [
     {
       id: 'm1',
       title: 'This is a first meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Netflix-avatar.png/600px-Netflix-avatar.png',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
       address: 'Some address 5, 12345 Some City',
       description: 'This is a first meetup',
     },
     {
       id: 'm2',
       title: 'This is a second meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Netflix-avatar.png/600px-Netflix-avatar.png',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
       address: 'Some address 10, 12345 Some City',
       description: 'This is a second meetup',
     },
     {
       id: 'm3',
       title: 'This is a third meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Netflix-avatar.png/600px-Netflix-avatar.png',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
       address: 'Some address 15, 12345 Some City',
       description: 'This is a third meetup',
     },
-  ]
+  ];
   return (
     <>
-    <MeetupList meetups={DUMMY_MEETUPS} />
+      
+        <MeetupList meetups={DUMMY_MEETUPS} />
     </>
-  )
-}
+  );
+};
 
-export default index
+export default index;
