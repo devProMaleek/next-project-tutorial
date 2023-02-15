4f3e694b0487c88ae1627c0c5b860f4b5d247e5c
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-15 10:00:08 +0100

    getStatiscPath and Fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,3 +13,29 @@ const MeetupDetailsPage = () => {
 };
 
 export default MeetupDetailsPage;
+
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      { params: { meetupId: 'm1' } },
+      { params: { meetupId: 'm2' } },
+    ],
+  };
+}
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Flag_of_the_United_States.svg',
+        id: meetupId,
+        title: 'A First Meetup',
+        address: 'Some Address 5, 12345 Some City',
+        description: 'This is a first meetup!',
+      },
+    },
+  };
+}
